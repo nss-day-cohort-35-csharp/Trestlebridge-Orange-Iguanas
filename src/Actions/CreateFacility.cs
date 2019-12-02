0b9bf683c985f2097f0ef110bcb07f1caa720125
David Cornish <davidcornish@Davids-MacBook-Pro.local>
2019-12-02 15:26:10 -0600

    grazing away

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,25 +3,32 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class CreateFacility {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
+namespace Trestlebridge.Actions
+{
+    public class CreateFacility
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Grazing field");
+            Console.WriteLine("2. Plowed field");
 
-            Console.WriteLine ();
-            Console.WriteLine ("Choose what you want to create");
+            Console.WriteLine();
+            Console.WriteLine("Choose what you want to create");
+            Console.Write("> ");
 
-            Console.Write ("> ");
-            string input = Console.ReadLine ();
-
+            string input = Console.ReadLine();
             switch (Int32.Parse(input))
             {
                 case 1:
                     farm.AddGrazingField(new GrazingField());
+                    Console.WriteLine("Grazing Field Created!!!!!");
+                    Console.WriteLine("Press Return to go back to the Main Menu");
+                    Console.ReadLine();
+
                     break;
                 default:
                     break;
+
             }
         }
     }
