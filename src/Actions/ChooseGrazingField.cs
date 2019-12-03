faa9797ae410995f5e0ea9a524b253846490928c
lauren maxwell <laurenelizamax@icloud.com>
2019-12-03 10:30:44 -0600

    first commit

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -14,7 +14,7 @@ namespace Trestlebridge.Actions
 
             for (int i = 0; i < farm.GrazingFields.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. Grazing Field");
+                Console.WriteLine($"{i + 1}. Grazing Field ({farm.GrazingFields[i].AnimalCount} {(farm.GrazingFields[i].AnimalCount == 1 ? "animal" : "animals")})");
             }
 
             Console.WriteLine();
